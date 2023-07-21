d88a979b46126e28faa404eff26bf6e36afe7385
Ana Beatriz Nunes <beatriznunez601@gmail.com>
2023-07-20 23:24:26 -0300

    feat: adding transaction repository in database module

--- src/config/database/database.module.ts
+++ src/config/database/database.module.ts
@@ -2,7 +2,8 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from 'src/repository/user.repository';
 import { CategoryRepository } from 'src/repository/category.repository';
-import { BankAccountRepository } from 'src/repository/bank-accounts.repository';
+import { BankAccountRepository } from 'src/repository/bank-account.repository';
+import { TransactionRepository } from 'src/repository/transaction.repository';
 
 @Global()
 @Module({
@@ -11,7 +12,13 @@ import { BankAccountRepository } from 'src/repository/bank-accounts.repository';
     UserRepository,
     CategoryRepository,
     BankAccountRepository,
+    TransactionRepository,
   ],
-  exports: [UserRepository, CategoryRepository, BankAccountRepository],
+  exports: [
+    UserRepository,
+    CategoryRepository,
+    BankAccountRepository,
+    TransactionRepository,
+  ],
 })
 export class DatabaseModule {}
